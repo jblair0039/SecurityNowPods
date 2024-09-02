65a84d7bb26adb3e6c76131889881e44a8ea647e
jblair0039 <44126541+jblair0039@users.noreply.github.com>
2024-09-02 16:04:51 -0400

    Update SNDownloader.ps1

--- SNDownloader.ps1
+++ SNDownloader.ps1
@@ -2,7 +2,7 @@
 # Place URL's in the column named "url" and filenames in a column named "output" in the episodes.csv file
 #Jordan Blair, 2024
 
-$csvData = Import-Csv "$ENV:USERPROFILE\Downloads\episodes.csv"
+$csvData = Import-Csv "$ENV:USERPROFILE\Downloads\SNDownloader\episodes.csv"
 
 foreach($now in $csvData){
 
